3d7244af05a63deae8d29f3e7908df5c05a01b14
koralhasson <koralhasson99@gmail.com>
2023-02-26 21:33:26 +0200

    teacher login and insert

---
+++ Project_ServerSide/Models/DAL/Teachers_DBservices .cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.Xml.Linq;
+using Project_ServerSide.Models;
+using System.Numerics;
+
+namespace Project_ServerSide.Models.DAL
+{
+    public class Teachers_DBservices
+    {
+        public SqlDataAdapter da;
+        public DataTable dt;
+
+
+        //--------------------------------------------------------------------------------------------------
+        // This method creates a connection to the database according to the connectionString name in the web.config 
+        //--------------------------------------------------------------------------------------------------
+        public SqlConnection connect(String conString)
+        {
+
+            // read the connection string from the configuration file
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json").Build();
+            string cStr = configuration.GetConnectionString("myProjDB");
+            SqlConnection con = new SqlConnection(cStr);
+            con.Open();
+            return con;
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // This method inserts a teacher to the Teachers table 
+        //--------------------------------------------------------------------------------------------------
+        public int Insert(Teacher teacher)
+        {
+
+
+            SqlConnection con;
+            SqlCommand cmd;
+
+            try
+            {
+                con = connect("myProjDB"); // create the connection
+            }
+            catch (Exception ex)
+            {
+                // write to log
+                throw ex;
+            }
+
+            cmd = CreateInsertTeacherstCommandSP("spInsertTeachers", con, teacher);
+
+            try
+            {
+                int numEffected = cmd.ExecuteNonQuery(); // execute the command
+                return numEffected;
+            }
+            catch (Exception ex)
+            {
+                // write to log
+                throw ex;
+            }
+
+            finally
+            {
+                if (con != null)
+                {
+                    // close the db connection
+                    con.Close();
+                }
+            }
+
+        }
+
+
+        //---------------------------------------------------------------------------------
+        // Create the SqlCommand InsertCommand
+        //---------------------------------------------------------------------------------
+        private SqlCommand CreateInsertTeacherstCommandSP(String spName, SqlConnection con, Teacher teacher)
+        {
+
+            SqlCommand cmd = new SqlCommand(); // create the command object
+
+            cmd.Connection = con;              // assign the connection to the command object
+
+            cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+            cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be stored procedure
+
+            cmd.Parameters.AddWithValue("@teacherId", teacher.TeacherId);
+            cmd.Parameters.AddWithValue("@password", teacher.Password);
+            cmd.Parameters.AddWithValue("@firstName", teacher.FirstName);
+            cmd.Parameters.AddWithValue("@lastName", teacher.LastName);
+            cmd.Parameters.AddWithValue("@phone", teacher.Phone);
+            cmd.Parameters.AddWithValue("@email", teacher.Email);
+            cmd.Parameters.AddWithValue("@pictureUrl", teacher.PictureUrl);
+
+
+            return cmd;
+        }
+
+        // This method - login 
+        //---------------------------------------------------------------------------------
+        public Teacher Login(Teacher teacher)
+        {
+
+            SqlConnection con;
+            SqlCommand cmd;
+
+            try
+            {
+                con = connect("myProjDB"); // create the connection
+            }
+            catch (Exception ex)
+            {
+                // write to log
+                throw (ex);
+            }
+
+
+            cmd = CreateLoginCommandSP("spLoginTeachers", con, teacher);// create the command
+            Teacher u = new Teacher();
+            try
+            {
+                SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dataReader.Read())
+                {
+                    u.TeacherId = Convert.ToInt32(dataReader["teacherId"]); 
+                    u.Password = dataReader["Password"].ToString();
+                    u.Email = dataReader["Email"].ToString();
+                    u.FirstName = dataReader["Firstname"].ToString();
+                    u.LastName = dataReader["Lastname"].ToString();
+                    u.Phone = Convert.ToDouble(dataReader["Phone"]);
+                    u.PictureUrl = dataReader["PictureUrl"].ToString();
+                    u.GroupId = Convert.ToInt32(dataReader["groupId"]);
+                }
+                return u;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            finally
+            {
+                if (con != null)
+                {
+                    // close the db connection
+                    con.Close();
+                }
+            }
+
+        }
+
+        // Create the Login SqlCommand
+        //---------------------------------------------------------------------------------
+        private SqlCommand CreateLoginCommandSP(String spName, SqlConnection con, Teacher teacher)
+        {
+
+            SqlCommand cmd = new SqlCommand(); // create the command object
+
+            cmd.Connection = con;              // assign the connection to the command object
+
+            cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+            cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be stored procedure
+
+
+            cmd.Parameters.AddWithValue("@teacherId", teacher.TeacherId); 
+            cmd.Parameters.AddWithValue("@Password", teacher.Password);
+
+
+            return cmd;
+        }
+
+    }
+}
