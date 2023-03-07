2c67311fee0d6edd87db80a57f23dd7d71f10a08
Barel077 <125794992+rupcgroup27Proj@users.noreply.github.com>
2023-03-07 11:00:19 +0200

    Added Shahar's files

---
+++ Project_ServerSide/Controllers/TagsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_ServerSide.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Project_ServerSide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        // GET: api/<TagsController>
+        [HttpGet]
+        public List<Tag> Get()
+        {
+            return Tag.GetTags();
+        }
+
+        // GET api/<TagsController>/5
+        [HttpGet("{postId}")]
+        public List<Tag> Get(int postId)
+        {
+            return Tag.ReadTagInPost(postId);
+        }
+
+        // POST api/<TagsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TagsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TagsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Project_ServerSide/Models/FavList.cs
@@ -0,0 +1,34 @@
+﻿using Project_ServerSide.Models.DAL;
+using System.Net;
+
+namespace Project_ServerSide.Models
+{
+    public class FavList
+    {
+        int studentId;
+        int postId;
+
+        public int StudentId { get => studentId; set => studentId = value; }
+        public int PostId { get => postId; set => postId = value; }
+
+
+        public static List<FavList> ReadByStudentId(int studentId)
+        {
+            FavList_DBservices dbs = new FavList_DBservices();
+            return dbs.FavListByStudentId(studentId);
+        }
+
+        public static bool Insert(int studentId ,int postId)
+        {
+            FavList_DBservices dbs = new FavList_DBservices();
+            return dbs.InsertFavPost(studentId, postId);
+
+        }
+
+        public int Delete(int studentId, int postId)
+        {
+            FavList_DBservices dbs = new FavList_DBservices();
+            return dbs.DeletePostFromListFav(studentId, postId);
+        }
+    }
+}
