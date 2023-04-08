67f3e3a50e23b142cc34715e79496bb86f082fd1
koralhasson <koralhasson99@gmail.com>
2023-04-08 14:53:36 +0300

    excelfix

--- Project_ServerSide/Controllers/ExcelController.cs
+++ Project_ServerSide/Controllers/ExcelController.cs
@@ -1,125 +1,91 @@
-﻿﻿// Add a reference to System.Data.SqlClient
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
-using System.Text.RegularExpressions;
-using ExcelDataReader;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Project_ServerSide.Models;
 
-// test comment
-public class ExcelController : ControllerBase
+namespace ExcelUpload.Controllers
 {
-    [HttpPost("api/excel/upload")]
-    public IActionResult UploadExcelFile(IFormFile file)
+    [ApiController]
+    public class ExcelController : ControllerBase
     {
-        // Parse the Excel file---note
-        var dataTable = ParseExcelFile(file);
-
-        // Insert the data into the database
-        InsertDataIntoDatabase(dataTable);
-        return Ok();
-    }
-
-    private DataTable ParseExcelFile(IFormFile file)
-    {
-        var dataTable = new DataTable();
-
-        using (var stream = new MemoryStream())
+        [HttpPost("api/excel/upload")]
+        public IActionResult UploadExcelFile(IFormFile file)
         {
-            file.CopyTo(stream);
-            var excelReader = ExcelReaderFactory.CreateReader(stream);
+            // Set the license context for EPPlus
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-            // Read the first sheet from the Excel file
-            var dataSet = excelReader.AsDataSet(new ExcelDataSetConfiguration
-            {
-                ConfigureDataTable = _ => new ExcelDataTableConfiguration
-                {
-                    UseHeaderRow = true
-                }
-            });
-            dataTable = dataSet.Tables[0];
+            // Parse the Excel file
+            var dataTable = ParseExcelFile(file);
+
+            // Insert the data into the database
+            InsertDataIntoDatabase(dataTable);
+
+            return Ok();
         }
 
-        return dataTable;
-    }
-
-    private void InsertDataIntoDatabase(DataTable dataTable)
-    {
-        // Connect to the database
-        var connectionString = "Data Source=Media.ruppin.ac.il;Initial Catalog=igroup127_prod; User ID=igroup127; Password=igroup127_29833";
-        using (var connection = new SqlConnection(connectionString))
+        private DataTable ParseExcelFile(IFormFile file)
         {
-            connection.Open();
+            var dataTable = new DataTable();
 
-            using (var command = new SqlCommand("spInsertStudent", connection))
+            using (var package = new ExcelPackage(file.OpenReadStream()))
             {
-                command.CommandType = CommandType.StoredProcedure;
+                var worksheet = package.Workbook.Worksheets[0];
+                var cellCount = worksheet.Dimension.End.Column;
+                var rowCount = worksheet.Dimension.End.Row;
 
-                foreach (DataRow row in dataTable.Rows)
+                // Set up the columns in the data table
+                for (var cell = 1; cell <= cellCount; cell++)
                 {
-                    command.Parameters.Clear();
+                    dataTable.Columns.Add(worksheet.Cells[1, cell].Value.ToString());
+                }
 
-                    command.Parameters.Add("@Id", SqlDbType.Int).Value = row["StudentId"];
-                    command.Parameters.Add("@password", SqlDbType.NVarChar).Value = row["Password"];
-                    command.Parameters.Add("@firstName", SqlDbType.NVarChar).Value = row["FirstName"];
-                    command.Parameters.Add("@lastName", SqlDbType.NVarChar).Value = row["LastName"];
-                    command.Parameters.Add("@phone", SqlDbType.NVarChar).Value = row["Phone"];
-                    command.Parameters.Add("@email", SqlDbType.VarChar).Value = row["Email"];
-                    command.Parameters.Add("@parentPhone", SqlDbType.VarChar).Value = row["ParentPhone"];
-                    command.Parameters.Add("@pictureUrl", SqlDbType.VarChar).Value = row["PictureUrl"];
-                    command.Parameters.Add("@groupId", SqlDbType.Int).Value = row["GroupId"];
+                // Add the rows to the data table
+                for (var row = 2; row <= rowCount; row++)
+                {
+                    var dataRow = dataTable.NewRow();
+                    for (var cell = 1; cell <= cellCount; cell++)
+                    {
+                        dataRow[cell - 1] = worksheet.Cells[row, cell].Value;
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
 
-                 
+            return dataTable;
+        }
 
-                    command.ExecuteNonQuery();
+        private void InsertDataIntoDatabase(DataTable dataTable)
+        {
+            // Connect to the database
+            var connectionString = "Data Source=Media.ruppin.ac.il;Initial Catalog=igroup127_prod; User ID=igroup127; Password=igroup127_29833";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand("spInsertStudent", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        command.Parameters.Clear();
+
+                        command.Parameters.Add("@Id", SqlDbType.Int).Value = row["StudentId"];
+                        command.Parameters.Add("@password", SqlDbType.NVarChar).Value = row["Password"];
+                        command.Parameters.Add("@firstName", SqlDbType.NVarChar).Value = row["FirstName"];
+                        command.Parameters.Add("@lastName", SqlDbType.NVarChar).Value = row["LastName"];
+                        command.Parameters.Add("@phone", SqlDbType.NVarChar).Value = row["Phone"];
+                        command.Parameters.Add("@email", SqlDbType.VarChar).Value = row["Email"];
+                        command.Parameters.Add("@parentPhone", SqlDbType.VarChar).Value = row["ParentPhone"];
+                        command.Parameters.Add("@pictureUrl", SqlDbType.VarChar).Value = row["PictureUrl"];
+                        command.Parameters.Add("@groupId", SqlDbType.Int).Value = row["GroupId"];
+
+                        command.ExecuteNonQuery();
+                    }
                 }
             }
         }
-
-        //using (var connection = new SqlConnection(connectionString))
-        //{
-        //    connection.Open();
-
-
-        // Create a SqlCommand object with parameterized SQL to insert the data
-        //var commandText = "INSERT INTO Students ([Id], [password],[firstName], [lastName], [phone], [email], [parentPhone], [pictureUrl]) VALUES (@Id,  @password,  @firstName, @lastName, @phone, @email, @parentPhone,@pictureUrl) ";
-
-        //using (var command = new SqlCommand(commandText, connection))
-        //{
-        //    // Add parameters for each column in the table
-        //    command.Parameters.Add("@Id", SqlDbType.Int);
-        //    command.Parameters.Add("@password", SqlDbType.NVarChar);
-        //    command.Parameters.Add("@firstName", SqlDbType.NVarChar);
-        //    command.Parameters.Add("@lastName", SqlDbType.NVarChar);
-        //    command.Parameters.Add("@phone", SqlDbType.NVarChar);
-        //    command.Parameters.Add("@email", SqlDbType.VarChar);
-        //    command.Parameters.Add("@parentPhone", SqlDbType.VarChar);
-        //    command.Parameters.Add("@pictureUrl", SqlDbType.VarChar);
-
-
-        //    // Add more parameters for each column in your table
-
-        //    // Loop through the rows of the DataTable and insert each row into the database
-        //    foreach (DataRow row in dataTable.Rows)
-        //    {
-        //        command.Parameters["@Id"].Value = row["Id"];
-        //        command.Parameters["@password"].Value = row["Password"];
-        //        command.Parameters["@firstName"].Value = row["FirstName"];
-        //        command.Parameters["@lastName"].Value = row["LastName"];
-        //        command.Parameters["@phone"].Value = row["Phone"];
-        //        command.Parameters["@email"].Value = row["Email"];
-        //        command.Parameters["@parentPhone"].Value = row["ParentPhone"];
-        //        command.Parameters["@pictureUrl"].Value = row["PictureUrl"];
-
-
-        //        // Set the value of each parameter for each column in your table
-        //        // ...
-
-
-        //        command.ExecuteNonQuery();
-        //    }
     }
 }
-    
